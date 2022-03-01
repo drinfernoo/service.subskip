21c0842eade715a5104113d6b750c364d5159b7c
drinfernoo <drinfernoo@yahoo.com>
2022-02-28 20:31:01 -0800

    :construction: - Allow local skip point configuration

--- resources/lib/local_points.py
+++ resources/lib/local_points.py
@@ -13,7 +13,7 @@ _addon_data = xbmcvfs.translatePath(settings.get_addon_info("profile"))
 
 def get_local_points():
     player = xbmc.Player()
-    # import web_pdb; web_pdb.set_trace()
+
     video_meta = player.getVideoInfoTag()
     imdb_id = str(video_meta.getIMDBNumber())
     season = str(video_meta.getSeason())
@@ -21,18 +21,29 @@ def get_local_points():
 
     data_points = {}
     file_path = os.path.join(_addon_data, "skip_points", "{}.json".format(imdb_id))
-    if not os.path.exists:
+    if not os.path.exists(file_path):
         tools.log("No local skip points found for the playing file.", "info")
         return None
-        
+
     with open(file_path) as f:
         data_points = json.load(f)
 
-    season_points = data_points.get(season, {})
+    show_points = data_points.get("show", {})
+    season_points = data_points.get("seasons", {}).get(season, {})
     episode_points = season_points.get(episode, {})
-    
-    intro = []
-    if episode_points:
-        intro.append((time.fromisoformat(episode_points.get("intro", {}).get("start", "00:00:00,000000")), time.fromisoformat(episode_points.get("intro", {}).get("end", "00:00:00,000000"))))
 
-    return intro
+    intro = episode_points.get(
+        "intro", season_points.get("intro", show_points.get("intro"))
+    )
+    # outro = episode_points.get("outro", season_points.get("outro", show_points.get("outro")))
+
+    intro_times = []
+    if intro:
+        intro_times.append(
+            (
+                time.fromisoformat(intro.get("start", "00:00:00,000000")),
+                time.fromisoformat(intro.get("end", "00:00:00,000000")),
+            )
+        )
+
+    return intro_times
