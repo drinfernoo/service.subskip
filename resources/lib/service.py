e7bd306b2343272dacf22d0401f2ce96ab4f3acc
drinfernoo <drinfernoo@yahoo.com>
2022-03-04 15:58:30 -0800

    :zap: - Reduce calls to player

--- resources/lib/service.py
+++ resources/lib/service.py
@@ -21,22 +21,20 @@ def run():
             # Abort was requested while waiting. We should exit
             break
 
-        if player.isPlayingVideo() and player.get_intro() is not None:
+        intro = player.get_intro()
+        if intro is not None:
+            dialog = player.get_dialog()
             current_time = player.getTime()
-            player.get_dialog().setProperty(
-                "skip_time", str(tools.convert_time_to_seconds(player.intro[1]))
+            dialog.setProperty(
+                "skip_time", str(tools.convert_time_to_seconds(intro[1]))
             )
 
             if (
-                player.get_intro() is not None
-                and not player.get_dialog().isCancelled()
-                and current_time >= tools.convert_time_to_seconds(player.intro[0])
+                not dialog.isCancelled()
+                and current_time >= tools.convert_time_to_seconds(intro[0])
                 and xbmcgui.getCurrentWindowDialogId() < 13000
             ):
-                player.get_dialog().show()
-            if (
-                player.get_intro() is not None
-                and current_time >= tools.convert_time_to_seconds(player.intro[1])
-            ) or player.get_intro() is None:
-                player.get_dialog().close()
+                dialog.show()
+            if current_time >= tools.convert_time_to_seconds(intro[1]):
+                dialog.close()
                 player.reset_intro()
